1b0e0524409aab3e365ed5e118958a4e6c3d663a
Shelsy <shelsyangelik@gmail.com>
2024-11-01 16:13:35 -0500

    V1 Forma a recurso

---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::apiResource('v1/posts', App\Http\Controllers\Api\V1\PostController::class)->only('show');
